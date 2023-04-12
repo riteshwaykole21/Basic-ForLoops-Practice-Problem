2361539278651433d2cda3fca6e95b258fd082d6
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-04-12 11:16:46 +0530

    [Ritesh]add : Factorial Program

---
+++ BasicExercise/Pattern.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicExercise
+{
+    public class Pattern
+    {
+        public void Number()
+        {
+            int k = 1;
+            Console.WriteLine("Enter The Number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i <= Number; i++)
+            {
+                k = k * i;
+            }
+            Console.WriteLine("The Factorial of {0} is {1}", Number, k);
+        }
+    }
+}
---
+++ BasicExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 : Factorial Program");
+            Console.WriteLine("Enter a Option");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Pattern pattern = new Pattern();
+                    pattern.Number();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
